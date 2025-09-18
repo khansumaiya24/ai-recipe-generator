d1dbb21b2472e7a28de3177dc785a0a50dfdb1cb
Sumaiya khan <t3khsu00@students.oamk.fi>
2025-09-19 00:08:37 +0300

    Configure Amplify in entry point

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'   // because outputs is at repo root
+Amplify.configure(outputs)
+
+
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
